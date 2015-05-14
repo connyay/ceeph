21edb2b12bd405a953853263c3517d1e1d8c95e7
Connor Hindley <connor@redhat.com>
2015-05-14 10:25:35 -0400

    add tar extraction

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 'use strict';
-require('./lib/bootstrap');
+
+var args = process.argv.slice(2);
+var uploadPath = args[0],
+    uploadUUID = args[1];
+
+function bootstrap() {
+    require('./lib/bootstrap');
+}
+
+if (!uploadPath || !uploadUUID) {
+    return bootstrap();
+}
+var extractor = require('./lib/extract');
+extractor(uploadPath, uploadUUID)
+    .then(bootstrap)
+    .catch(function (err) {
+        console.log('Error Extracting File', err);
+    });
